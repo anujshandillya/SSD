bcfcbdbd371524e32f7be618da2ca459809e7ccc
anujshandillya <anujsharmawork11@gmail.com>
2026-08-15 12:13:12 +0530

    add: login form

--- Assignment 2(git)/login/login.cpp
+++ Assignment 2(git)/login/login.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+class User {
+    string userEmail;
+    string username;
+    string password;
+public:
+    User(const string userEmail, const string username, const string password) {
+        this->userEmail = userEmail;
+        this->username = username;
+        this->password = password;
+    }
+    string getUsername() {
+        return this->username;
+    }
+    string getEmail() {
+        return this->userEmail;
+    }
+};
+
 ssize_t main() {
     cout << "this is a C++ login file for the login branch." << endl;
+    cout << "Enter your email: ";
+    string email;cin >> email;
+    cout << "Enter your username: ";
+    string username;cin >> username;
+    cout << "Enter your password: ";
+    string password;cin >> password;
+
+
 }
