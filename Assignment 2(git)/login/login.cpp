33aa05fa54bc311581ab82abf15ff02e7172bd13
anujshandillya <anujsharmawork11@gmail.com>
2026-08-15 12:17:48 +0530

    add: login validation for a user

--- Assignment 2(git)/login/login.cpp
+++ Assignment 2(git)/login/login.cpp
@@ -7,6 +7,9 @@ class User {
     string userEmail;
     string username;
     string password;
+    string getPassword() {
+        return this->password;
+    }
 public:
     User(const string userEmail, const string username, const string password) {
         this->userEmail = userEmail;
@@ -19,6 +22,15 @@ public:
     string getEmail() {
         return this->userEmail;
     }
+
+    bool validateUser(const string userEmail, const string username, const string password) {
+        if(userEmail == getEmail()) {
+            if(password == getPassword()) {
+                return true;
+            }
+        }
+        return false;
+    }
 };
 
 ssize_t main() {
@@ -30,5 +42,6 @@ ssize_t main() {
     cout << "Enter your password: ";
     string password;cin >> password;
 
-
+    User *u = new User(email, username, password);
+    
 }
